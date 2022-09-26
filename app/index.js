026f64780ca0e290897c41d191ed991b7b44a275
Joseph L <josephsamuellee@alumni.ucsd.edu>
2022-09-25 17:37:08 -0700

    Testing LUT ISO-8601 Method
    Line two of GUI now testing (for several weeks) better power optimized
    method of calculating week number.
    
    Other fixes:
    * previously attempted to opdate on Monday 0000-0059 every minute, but
      trying to only update three times (Mondaya 0001, 0101, 0201)
    * table is being generated on google sheets object
      https://docs.google.com/spreadsheets/d/1_MvOg7xcOUsSj3Z8gQBGBvOf4d1zIz5CEkh5bEjMQgw/edit?usp=sharing

--- app/index.js
+++ app/index.js
@@ -8,6 +8,8 @@ import { today } from "user-activity";
 //battery
 import { battery } from "power";
 
+import { me as device } from "device"; //console
+
 // Update the clock every minute
 clock.granularity = "minutes";
 
@@ -16,6 +18,8 @@ const myTime = document.getElementById("myTime");
 const myDate = document.getElementById("myDate");
 const mySteps = document.getElementById("mySteps");
 const myDebug = document.getElementById("myDebug");
+const myWeekNum = document.getElementById("myWeekNum");
+const myMMDD = document.getElementById("myMMDD");
 
 //function to generate week num
 function current_week_num() {
@@ -47,10 +51,32 @@ function current_week_num() {
   }
 //set current week num on buildtime (so we don't have to wait until night)
 let buildtime_weeknum = current_week_num();
+let test_date = new Date();
+const weeknumArr = [
+  [0,0,1,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4,4,4,5],
+  [5,5,5,5,5,5,6,6,6,6,6,6,6,7,7,7,7,7,7,7,8,8,8,8,8,8,8,9,52,52,52],
+  [9,9,9,9,9,9,10,10,10,10,10,10,10,11,11,11,11,11,11,11,12,12,12,12,12,12,12,13,13,13,13],
+  [13,13,13,14,14,14,14,14,14,14,15,15,15,15,15,15,15,16,16,16,16,16,16,16,17,17,17,17,17,17,52],
+  [17,18,18,18,18,18,18,18,19,19,19,19,19,19,19,20,20,20,20,20,20,20,21,21,21,21,21,21,21,22,22],
+  [22,22,22,22,22,23,23,23,23,23,23,23,24,24,24,24,24,24,24,25,25,25,25,25,25,25,26,26,26,26,52],
+  [26,26,26,27,27,27,27,27,27,27,28,28,28,28,28,28,28,29,29,29,29,29,29,29,30,30,30,30,30,30,30],
+  [31,31,31,31,31,31,31,32,32,32,32,32,32,32,33,33,33,33,33,33,33,34,34,34,34,34,34,34,35,35,35],
+  [35,35,35,35,36,36,36,36,36,36,36,37,37,37,37,37,37,37,38,38,38,38,38,38,38,39,39,39,39,39,52],
+  [39,39,40,40,40,40,40,40,40,41,41,41,41,41,41,41,42,42,42,42,42,42,42,43,43,43,43,43,43,43,44],
+  [44,44,44,44,44,44,45,45,45,45,45,45,45,46,46,46,46,46,46,46,47,47,47,47,47,47,47,48,48,48,52],
+  [48,48,48,48,49,49,49,49,49,49,49,50,50,50,50,50,50,50,51,51,51,51,51,51,51,52,52,52,52,52,52]
+  ];
+console.log("month="+test_date.getMonth()+
+            "day="+(test_date.getDate()-1)+ 
+            "weeknum="+weeknumArr[test_date.getMonth()][test_date.getDate()-1]
+           );
+let buildtime_weeknum_alt = weeknumArr[test_date.getMonth()][test_date.getDate()-1];
 
+/* removed as of w38-7
 function updateSteps() {
   mySteps.text = today.adjusted.steps;
 }
+*/ 
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
@@ -70,12 +96,16 @@ clock.ontick = (evt) => {
   //myTime.text = `${hours}:${mins}`;
   
   //update week num on mondays 0100, where getday returns 0=sun through 6=sat
-  if ((today.getDay() === 1) && (today.getHours() === 0)) {
+  //if ((today.getDay() === 1) && (today.getHours() === 0)) {
+  if ((today.getDay() === 1) && (today.getHours() < 3) && (today.getMinutes()===1)) { //update this three times (because we saw bugs in when this is run)
     let buildtime_weeknum = current_week_num();
-    //let buildtime_weeknum = util.zeroPad(buildtime_weeknum)
+    buildtime_weeknum_alt = weeknumArr[today.getMonth()][today.getDate()-1];
   }
   myTime.text = `${hours}:${mins}`;
-  myDate.text = "W"+buildtime_weeknum+" "+util.zeroPad(today.getMonth()+1)+"."+util.zeroPad(today.getDate());
+  myMMDD.text = "mm"+util.zeroPad(today.getMonth()+1)+"dd"+util.zeroPad(today.getDate());
+  myDate.text = "W"+buildtime_weeknum+" old";
+  myWeekNum.text = "2022-W"+util.zeroPad(buildtime_weeknum_alt)+"-"+today.getDay(); //this is new w38-7
   myDebug.text = battery.chargeLevel + "%";
-  updateSteps();
+  //myDebug.text = "hello"
+  //updateSteps(); // removed as of w38-7
 }
